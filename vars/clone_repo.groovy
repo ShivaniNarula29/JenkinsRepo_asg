0e567aa0a31445f43a9618370c2bc88b30f6d2c3
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-02-05 01:36:40 +0530

    Update clone_repo.groovy

--- vars/clone_repo.groovy
+++ vars/clone_repo.groovy
@@ -1,4 +1,5 @@
-def call(codeBasePath,repoUrl,env) {
+def call(String codeBasePath,String repoUrl,String env) {
+    echo "enter git"
     git url: ${repoUrl}, branch: ${env}
     echo "Cloned repo from ${codeBasePath}"
 }
