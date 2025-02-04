fa0533d8c4dad0149a273e44d0e64f08d8d354bd
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-02-05 02:23:00 +0530

    Update ansible_deploy.groovy

--- vars/ansible_deploy.groovy
+++ vars/ansible_deploy.groovy
@@ -1,10 +1,11 @@
 def call(String environment, String codeBasePath, String actionMessage, String prometheusHost) {
     // Ensure Ansible environment is properly set up before running
     echo "${codeBasePath}/Ansible_29/Assignment5/playbook.yaml"
-    sh '''
-    export ANSIBLE_CONFIG=ansible.cfg
-    ansible-playbook -i /home/ubuntu/Ansible_29/Assignment5/prometheus_tool/tests/inventory /home/ubuntu/Ansible_29/Assignment5/playbook.yaml
-    '''
+       sh "ansible-playbook -i ${config.CODE_BASE_PATH}/inventory ${config.CODE_BASE_PATH}/playbook.yml"
+    // sh '''
+    // export ANSIBLE_CONFIG=ansible.cfg
+    // ansible-playbook -i /home/ubuntu/Ansible_29/Assignment5/prometheus_tool/tests/inventory /home/ubuntu/Ansible_29/Assignment5/playbook.yaml
+    // '''
     // Call Prometheus after playbook execution to monitor the deployment
     sh """
     curl -X POST -H 'Content-Type: application/json' \
