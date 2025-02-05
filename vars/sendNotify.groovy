39d77d1c1f9a7298d1be1edb69d947cb8444034b
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-02-05 17:40:51 +0530

    Update sendNotify.groovy

--- vars/sendNotify.groovy
+++ vars/sendNotify.groovy
@@ -1,14 +1,5 @@
-// def call(String channel, String message, String color = 'good') {
-//        slackSend(channel: channel,
-//                  message: message, 
-//                  color: color)
-// }
-    slackSend (
-        channel: channel,
-        color: color,
-        message: message,
-        tokenCredentialId: 'Slack_update', // Make sure this matches your token credential in Jenkins
-        iconEmoji: ':rocket:', // Optional: Change this if you'd like a different icon
-        username: 'Jenkins' // Optional: Change this to the bot name you want
-    )
+def call(String channel, String message, String color = 'good') {
+       slackSend(channel: channel,
+                 message: message, 
+                 color: color)
 }
