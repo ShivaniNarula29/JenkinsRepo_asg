2de4f03cc4a27fa251d46afce074d71c72a86c30
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-02-05 15:20:44 +0530

    Update ansible_execution_playbook.groovy

--- vars/ansible_execution_playbook.groovy
+++ vars/ansible_execution_playbook.groovy
@@ -1,6 +1,7 @@
-def call(String environment, String codeBasePath) {
+def call(String environment, String actionMessage, String codeBasePath) {
     // Ensure Ansible environment is properly set up before running
     echo "${codeBasePath}/Ansible_29/Assignment5/playbook.yaml"
     sh 'ls -lrt /home/ubuntu/Ansible_29/Assignment5/playbook.yaml'
     sh "ansible-playbook -i localhost ${codeBasePath}/Ansible_29/Assignment5/playbook.yaml" 
+    echo "${actionMessage}"
 }
